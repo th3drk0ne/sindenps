94cf19cf5ce3978c99288cc66ba0fd3e1c44e829
Karl Dunne <dunne.karl@gmail.com>
2026-01-26 13:29:26 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -375,7 +375,7 @@ sudo chown sinden:sinden /opt/lightgun-dashboard/index.html
 
 echo "=== Downloading dashboard logo from GitHub ==="
 sudo wget -O /opt/lightgun-dashboard/logo.png \
-  https://raw.githubusercontent.com/th3drk0ne/sindenps/refs/heads/main/Linux/opt/lightgun-dashboard/logo.png
+  https://raw.githubusercontent.com/th3drk0ne/sindenps/master/Linux/opt/lightgun-dashboard/logo.png
 sudo chown sinden:sinden /opt/lightgun-dashboard/logo.png
 
 echo "=== Creating Python virtual environment ==="
