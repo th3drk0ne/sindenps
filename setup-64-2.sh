84cb8b9344b29b8b0b3f087b5862459257cc26a7
Karl Dunne <dunne.karl@gmail.com>
2026-02-06 19:54:53 +0000

    Update setup-64-2.sh

--- setup-64-2.sh
+++ setup-64-2.sh
@@ -265,6 +265,9 @@ fetch_source() {
 configure_build() {
   cd "${SRC_DIR}"
   log "Configuring build for shared library install under ${PREFIX}..."
+	wget -O config.guess https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD
+	wget -O config.sub   https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD
+	chmod +x config.guess config.sub
   # Enable shared, disable static to produce libjpeg.so.8
   ./configure --prefix="${PREFIX}" --enable-shared --disable-static
   log "Configure completed."
