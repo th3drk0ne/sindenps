eb60fa768918dd9232b6d2b12c91130ba3478fa4
th3drk0ne <dunne.karl@gmail.com>
2026-06-15 11:57:47 +0100

    Update update-sindenps.sh

--- Linux/opt/sinden/update-sindenps.sh
+++ Linux/opt/sinden/update-sindenps.sh
@@ -20,6 +20,6 @@ trap 'rm -f "$LOCK"' EXIT
 
 log "=== SindenPS update started $(date) ===" >> "$LOG"
 
-VERSION=latest sudo -E bash -c "$(wget -qO- https://raw.githubusercontent.com/th3drk0ne/sindenps/master/setup.sh)" >> "$LOG" 2>&1
+VERSION=latest sudo -E bash -c "$(wget -qO- https://raw.githubusercontent.com/th3drk0ne/sindenps/master/setup-test.sh)" >> "$LOG" 2>&1
 
 log "=== SindenPS update finished $(date) ===" >> "$LOG"
