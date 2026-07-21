abf1bf0654b999147d5cf09d52a30dabc0b261d1
th3drk0ne <dunne.karl@gmail.com>
2026-07-21 09:14:26 +0100

    add SONY-ANALOG Firmware

--- setup.sh
+++ setup.sh
@@ -302,8 +302,9 @@ cd /home/sinden/Firmware
   wget --quiet --show-progress --https-only --timestamping \
     "https://github.com/th3drk0ne/sindenps/raw/refs/heads/main/Firmware/PSX/GCON45-NTSC.hex" \
     "https://github.com/th3drk0ne/sindenps/raw/refs/heads/main/Firmware/PSX/GCON45-PAL.hex" \
-	"https://github.com/th3drk0ne/sindenps/raw/refs/heads/main/Firmware/PSX/KONAMI.hex"	\
-	"https://github.com/th3drk0ne/sindenps/raw/refs/heads/main/Firmware/PSX/GCON45-PAL-DieHardTrilogy.hex"
+	"https://github.com/th3drk0ne/sindenps/raw/refs/heads/main/Firmware/PSX/KONAMI.hex" \
+	"https://github.com/th3drk0ne/sindenps/raw/refs/heads/main/Firmware/PSX/GCON45-PAL-DieHardTrilogy.hex" \
+	"https://github.com/th3drk0ne/sindenps/raw/refs/heads/main/Firmware/PSX/GCON45-SONY-ANALOG.hex"
 
 
 LOG="/var/log/platform-update.log"
