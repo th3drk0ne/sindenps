87f07f1402a7a7b6f3e17c16a88b470c2e89952d
th3drk0ne <dunne.karl@gmail.com>
2026-08-06 10:06:53 +0100

    Update fan_controller.py

--- fan-ctrl/fan_controller.py
+++ fan-ctrl/fan_controller.py
@@ -17,8 +17,8 @@ WAIT_TIME = 2
 CONFIG_FILE = "/opt/fan-controller/fan_curve.json"
 
 DEFAULT_FAN_CONFIG = {
-    "tempSteps": [63, 65, 75, 85],
-    "speedSteps": [65, 75, 85, 100],
+    "tempSteps": [63, 65, 70, 75, 80, 85],
+    "speedSteps": [65, 70, 75, 80, 90, 100],
     "minSpin": 63,
     "hystDrop": 5
 }
