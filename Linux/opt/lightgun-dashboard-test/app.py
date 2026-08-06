3eadf56b8f1f64c25f49c721a0faadbbe94894e4
th3drk0ne <dunne.karl@gmail.com>
2026-08-06 11:38:59 +0100

    Update app.py

--- Linux/opt/lightgun-dashboard-test/app.py
+++ Linux/opt/lightgun-dashboard-test/app.py
@@ -815,7 +815,7 @@ def api_platform():
                     "adapters": adapters,
                     "detecting": detecting,
                     "fan_supported": supports_fan_script(),
-                    "pi_model": get_pi_model()
+                    "pi_board": get_pi_board
                 })
 
         return jsonify({
