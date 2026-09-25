aa282dac75b68ecfa320e22bea38155a48b114ed
th3drk0ne <dunne.karl@gmail.com>
2026-09-25 11:03:51 +0100

    Update update-sindenps.sh

--- Linux/opt/sinden/update-sindenps.sh
+++ Linux/opt/sinden/update-sindenps.sh
@@ -23,8 +23,4 @@ trap 'rm -f "$LOCK"; echo "=== SindenPS update finished $(date) ==="' EXIT
 export PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
 # run update
-sudo -E bash -c '
-  VERSION=latest
-  wget -qO- https://raw.githubusercontent.com/th3drk0ne/sindenps/master/setup.sh | bash
-'
-
+wget -qO- https://raw.githubusercontent.com/th3drk0ne/sindenps/master/setup.sh | sudo bash
